bed28cc233bd6cfcef86510cc39973b8be879423
Kirill <syskirill.com>
2025-05-10 22:57:42 +0300

    Initial Commit

---
+++ ProfileSettings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileSettings
+//
+//  Created by Кирилл Сысоев on 10.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProfileSettingsTests/ProfileSettingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileSettingsTests.swift
+//  ProfileSettingsTests
+//
+//  Created by Кирилл Сысоев on 10.05.2025.
+//
+
+import Testing
+@testable import ProfileSettings
+
+struct ProfileSettingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
